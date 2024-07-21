fa1b1006b4314775ad0175d76c4457955dcf1ba7
Gemma <johndoe@example.com>
2024-07-21 21:33:59 +0200

    css variables updated

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FaHome } from 'react-icons/fa';
-import { BsBookmarkStarFill } from 'react-icons/bs';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaHome } from 'react-icons/fa'
+import { BsBookmarkStarFill } from 'react-icons/bs'
 import './NavBar.css'
 
 const NavBar: React.FC = () => {
   return (
-    <nav className="bg-primary p-4">
-      <div className="container-nav mx-auto flex">
-        <Link to="/" className="text-white font-bold text-xl flex items-center">
-          <FaHome className="icon-navbar" />
+    <nav className='bg-primary p-4'>
+      <div className='container-nav mx-auto flex'>
+        <Link to='/' className='text-white font-bold text-xl flex items-center'>
+          <FaHome className='icon-navbar-home' />
         </Link>
-        <Link to="/saved" className="text-white font-bold text-xl flex items-center tooltip-container">
-          <BsBookmarkStarFill className="icon-navbar" /> 
-          <span className="tooltip-text">Saved Books</span>
+        <Link
+          to='/saved'
+          className='text-white font-bold text-xl flex items-center tooltip-container'
+        >
+          <span className='tooltip-text'>Saved Books</span>
+          <BsBookmarkStarFill className='icon-navbar-saved' />
         </Link>
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default NavBar;
+export default NavBar
